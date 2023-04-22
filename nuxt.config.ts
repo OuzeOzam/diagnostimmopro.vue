bfe781a1ca5f26b4dd7e95d0b7f0901e332a7842
Ouze <ossama.benjemaa@gmail.com>
2023-04-22 14:16:58 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import { defineNuxtConfig } from 'nuxt/config'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@invictus.codes/nuxt-vuetify'
+    ],
+    vuetify: {
+        /* vuetify options */
+        vuetifyOptions: {
+          // @TODO: list all vuetify options
+        },
+        moduleOptions: {
+          /* nuxt-vuetify module options */
+          treeshaking: true,
+          useIconCDN: true,
+          /* vite-plugin-vuetify options */
+          styles: 'sass',
+          autoImport: true,
+        }
+    }
+})
