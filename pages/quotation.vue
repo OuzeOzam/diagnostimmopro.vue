049211f55881a42bba8b16846b32fbba1013a2bf
Ouze <ossama.benjemaa@gmail.com>
2023-05-01 02:52:40 +0200

    maj button iframe

--- pages/quotation.vue
+++ pages/quotation.vue
@@ -1,7 +1,7 @@
 <template>
     <FormIframe />
-    <div class="flex flex-col justify-center items-center gap-1 max-w-[752px] px-6">
-      <ButtonBlockSecondary to="tel:+33660393750">Appelez-nous</ButtonBlockSecondary>
-      <p class="text-base">Notre numéro : 06.60.39.37.50</p>
+    <div class="flex flex-col justify-center items-center gap-1 max-w-[752px] px-6 pb-10">
+        <ButtonBlockTertiary to="tel:+33660393750">Une question ? Contactez-nous</ButtonBlockTertiary>
+        <p class="text-base">Par téléphone au 06.60.39.37.50</p>
     </div>
 </template>
